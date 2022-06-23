23062518201644d012db643e6b6b8fa507eb4f46
Cristobal Silva <cristobal.cntcto@gmail.com>
2022-06-23 09:35:53 -0400

    Se agregaron nuevos componentes {header}{cards}

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Books-app';
+  public cards:Array<any>=[]
+
+  ngOnInit(): void
+  {
+    this.cards=
+    [
+      {
+        title:'Libro 1',
+        subtitle:'Subtitle 1'
+      },
+      {
+        title:'Libro 1',
+        subtitle:'Subtitle 1'
+      },
+      {
+        title:'Libro 1',
+        subtitle:'Subtitle 1'
+      },
+      {
+        title:'Libro 1',
+        subtitle:'Subtitle 1'
+      },
+      {
+        title:'Libro 1',
+        subtitle:'Subtitle 1'
+      },
+      {
+        title:'Libro 1',
+        subtitle:'Subtitle 1'
+      }
+    ]
+
+  }
 }
